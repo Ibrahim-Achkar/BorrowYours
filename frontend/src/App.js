3c9d621f7bcdd9aeacd5da7a8fefe36f2b1c6015
Development-Person <ibaemail@gmail.com>
2021-05-30 16:15:31 +1000

    fixed up redux-persist
    Moved store creation out of app.js and into index.js
    Moved persist config and persistor out of featuresReducer.js and into configureStore.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import { Provider } from 'react-redux';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+
 //app imports
-import { myStore } from './store/configureStore';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomeScreen from './screens/HomeScreen';
@@ -17,35 +14,28 @@ import ItemListScreen from './screens/ItemListScreen';
 import UsersScreen from './screens/UsersScreen';
 import ItemScreen from './screens/ItemScreen';
 
-const store = myStore();
-const persistor = persistStore(store);
-
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <Header />
-          <main className='py-3'>
-            <Container>
-              <Route path='/profile' component={ProfileScreen} />
-              <Route path='/login' component={LoginScreen} />
-              <Route path='/register' component={RegisterScreen} />
-              <Route path='/items/:id' component={ItemScreen} exact />
-              <Route path='/items' component={ItemListScreen} exact />
-              <Route
-                path='/items/page/:pageNumber'
-                component={ItemListScreen}
-                exact
-              />
-              <Route path='/users' component={UsersScreen} />
-              <Route path='/' component={HomeScreen} exact />
-            </Container>
-          </main>
-          <Footer />
-        </Router>
-      </PersistGate>
-    </Provider>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/items/:id' component={ItemScreen} exact />
+          <Route path='/items' component={ItemListScreen} exact />
+          <Route
+            path='/items/page/:pageNumber'
+            component={ItemListScreen}
+            exact
+          />
+          <Route path='/users' component={UsersScreen} />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,12 +1,25 @@
 //package imports
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import autoMergeLevel1 from 'redux-persist/lib/stateReconciler/autoMergeLevel1';
+import localForage from 'localforage';
+
 //app imports
 import reducer from './reducers/rootReducer';
 import api from './middleware/apiMiddleware';
 
-export const myStore = () => {
+const persistConfig = {
+  key: 'root',
+  storage: localForage,
+  whitelist: ['features'],
+  stateReconciler: autoMergeLevel1,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const myStore = () => {
   return configureStore({
-    reducer,
+    reducer: persistedReducer,
     middleware: [
       ...getDefaultMiddleware({
         serializableCheck: { ignoredActions: ['persist/PERSIST'] },
@@ -15,3 +28,5 @@ export const myStore = () => {
     ],
   });
 };
+
+export default myStore;
